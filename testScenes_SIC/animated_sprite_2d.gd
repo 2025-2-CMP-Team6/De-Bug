af27326ad7f2d88d63dca6e11eb24a4c5b22d6d7
trueblue222 <shinieechul2012@naver.com>
2025-11-26 11:39:54 +0900

    state 별 애니메이션 적용

--- testScenes_SIC/animated_sprite_2d.gd
+++ testScenes_SIC/animated_sprite_2d.gd
@@ -1,4 +1,15 @@
 extends AnimatedSprite2D
 
+var player: CharacterBody2D = null
+
 func _ready():
-	play("idle")
+	player = get_parent().get_parent() if get_parent() else null
+
+func _process(_delta):
+	match GameManager.state:
+		GameManager.State.IDLE : play("idle") 
+		GameManager.State.MOVE : play("run") 
+		GameManager.State.DASH : play("DASH") # 일단 임시로 Dash 애니메이션
+		GameManager.State.SKILL_CASTING : play("attack") # 스킬 캐스팅 시간에 따라 조정해야 할듯?
+	if player and not player.is_on_floor():
+		play("jump")
