a6ebc5b4eead3b4df72049de42a4b94c45d32e72
trueblue222 <shinieechul2012@naver.com>
2025-12-06 10:39:25 +0900

    Stage2 Parallax 배경 추가

---
+++ testScenes_SIC/Stage2/Stage2.gd
@@ -0,0 +1,7 @@
+extends World
+
+func _on_portal_body_entered(body):
+	if body.is_in_group("player"):
+		print("플레이어가 포탈에 진입했습니다!")
+		# 여기에 포탈 이동 로직을 추가하세요
+		get_tree().change_scene_to_file("res://testScenes_SIC/Stage3/Stage3.tscn")
